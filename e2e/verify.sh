6914c58904d2fa976bd9067927b5381d57de5e37
tamiroh <tamiroh71@gmail.com>
2026-09-23 20:04:09 +0900

    Add PHP build baseline in e2e

---
+++ e2e/verify.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+
+cd /opt/php-src
+make -j2
+./sapi/cli/php -n -v
+./sapi/cli/php -n -r '
+    if (PHP_VERSION !== "8.5.0" || PHP_SAPI !== "cli" || 6 * 7 !== 42) {
+        exit(1);
+    }
+    echo "PHP build smoke test passed\n";
+'
