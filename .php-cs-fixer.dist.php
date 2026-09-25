669afa7af464d821ea1b95c2280a8b73afa31525
tamiroh <tamiroh71@gmail.com>
2026-09-25 19:35:35 +0900

    Install PHP CS Fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@auto' => true,
+        '@auto:risky' => true
+    ])
+    ->setFinder(
+        (new Finder())
+            ->in(__DIR__)
+    );
